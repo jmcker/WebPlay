8cb7488929c3c48a540271c258ce08b63e866022
Jack <jmcker@outlook.com>
2017-10-12 01:29:38 -0400

    Update callbacks for renaming production

--- js/code.js
+++ js/code.js
@@ -159,21 +159,27 @@ function closeTitle(saveChanges) {
             return;
         } else {
             // Update production file, production directory, display text, and window hash
-            renameFile("/" + display.innerHTML + "/" + display.innerHTML + ".wpjs", ".", text.value + ".wpjs");
-            renameFile("/" + display.innerHTML, "/", text.value, function() {
-                filer.cd("/" + text.value, function() {
+            renameFile("/" + display.innerHTML + "/" + display.innerHTML + ".wpjs", ".", text.value + ".wpjs", function() {
 
-                    onscreenInfo("Successfully renamed production \"" + display.innerHTML + "\" to \"" + text.value + "\"");
+                // Rename parent folder after production file has been successfully renamed
+                renameFile("/" + display.innerHTML, "/", text.value, function() {
 
-                    // Wait to change the text values until everything completes successfully
-                    display.innerHTML = text.value;
-                    window.location.hash = text.value;
+                    // Move into the renamed folder after it has been successfully renamed
+                    filer.cd("/" + text.value, function() {
+
+                        onscreenInfo("Successfully renamed production \"" + display.innerHTML + "\" to \"" + text.value + "\"");
+
+                        // Wait to change the text values until everything completes successfully
+                        display.innerHTML = text.value;
+                        window.location.hash = text.value;
                 
-                }, function() {
-                    alert("An error occurred while renaming the production. Please manually check or change the name of the production folder and the .wpjs file.");
-                    return;
+                    }, function() {
+                        alert("An error occurred while renaming the production. Please manually check or change the name of the production folder and the .wpjs file.");
+                        return;
+                    });
                 });
             });
+            
         }
     }
 
