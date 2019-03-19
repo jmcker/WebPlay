5b879673d3c642e15473ad1670dec9e2b1f3694e
Jack McKernan <jmcker@outlook.com>
2019-03-19 16:10:45 -0400

    Connect preview window close event

--- src/app/modules/file-browser/components/file-preview/file-preview.component.ts
+++ src/app/modules/file-browser/components/file-preview/file-preview.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
+import { LogService } from '@app/_services/log.service';
+import { Key } from 'ts-keycode-enum';
 
 @Component({
-  selector: 'app-file-preview',
-  templateUrl: './file-preview.component.html',
-  styleUrls: ['./file-preview.component.css']
+    selector: 'app-file-preview',
+    templateUrl: './file-preview.component.html',
+    styleUrls: ['./file-preview.component.css']
 })
 export class FilePreviewComponent implements OnInit {
 
-  constructor() { }
+    /**
+     * Signal that the preview window should be closed.
+     * Value of boolean argument does not change behavior.
+     */
+    @Output() close = new EventEmitter<boolean>();
 
-  ngOnInit() {
-  }
+    constructor(
+        private logServ: LogService
+    ) { }
+
+    ngOnInit() {
+    }
+
+    /**
+     * Listen for preview key events.
+     * @param event Key event
+     */
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        if (event.keyCode === Key.Escape) {
+            this.doClose();
+        }
+    }
+
+    /**
+     * Signal that the preview window should be closed.
+     */
+    doClose() {
+        this.logServ.debug('FilePreviewComp:\t Close event emitted.');
+        this.close.emit(true);
+    }
 
 }
